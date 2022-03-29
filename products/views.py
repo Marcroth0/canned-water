dc15a7a8f34df4a12e60224dd629354f1bd8adb0
Marcus Rothoff <marcusrothoff@hotmail.com>
2022-03-30 00:45:25 +0200

    Fixed broken img in json products, added style in css for margin-top, and added products in products.html

--- products/views.py
+++ products/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .models import Product
 
 def products(request):
-    return render(request, 'products/products.html')
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+    return render(request, 'products/products.html', context)
