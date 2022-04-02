5569f2849496fd819c3a6aa61d53f94046270531
Marcus Rothoff <marcusrothoff@hotmail.com>
2022-04-02 21:18:03 +0200

    Remove unnecessary code from add_to_bag and add session request to bag_contents

--- bag/views.py
+++ bag/views.py
@@ -10,7 +10,6 @@ def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
@@ -19,5 +18,4 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
