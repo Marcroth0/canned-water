47b8d45aaa1527f6b778b7f3ec7d695156f41f08
Marcus Rothoff <marcusrothoff@hotmail.com>
2022-04-05 17:47:40 +0200

    added checkout confirmationg logic

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
+    path('checkout_confirmation/<order_number>', views.checkout_confirmation, name='checkout_confirmation'),
 ]
