244ba278ee7696b23042c52ea3e29fd6450f544a
Marcus Rothoff <marcusrothoff@hotmail.com>
2022-04-09 04:01:28 +0200

    add 'add product' view

--- products/views.py
+++ products/views.py
@@ -79,3 +79,24 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+def add_product(request):
+    """ Add a product to the store """
+    
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('add_product'))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+        
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
