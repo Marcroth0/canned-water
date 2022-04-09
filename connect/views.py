0dfcc7d94dd00fc15193de5dd0a45b471ff07d88
Marcus Rothoff <marcusrothoff@hotmail.com>
2022-04-09 22:20:52 +0200

    Add views and template to connect

--- connect/views.py
+++ connect/views.py
@@ -1,8 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+from django.contrib import messages
+from profiles.models import UserProfile
 
 def contact_us(request):
     """View to let users send email to owner of site """
 
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,
+                             "Thank you for your message,\
+                              we will be in touch soon")
+            return redirect("contact")
+        else:
+            messages.error(request,
+                           "Something went wrong,\
+                            Please try again")
+            return redirect("contact")
+    else:
+        if request.user.is_authenticated:
+            try:
+                profile_email = UserProfile.objects.get(user=request.user)
+                form = ContactForm(initial={
+                    "email": profile_email.user.email
+                })
+            except UserProfile.DoesNotExist:
+                form = ContactForm()
+        else:
+            form = ContactForm()
+
     template = "connect/contact.html"
+    context = {
+        'form': form,
+    }
     
-    return render(request, template)
+    return render(request, template, context)
