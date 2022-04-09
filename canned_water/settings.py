39a9798b6f4ff6d5f49bd89158374fb4dc25a2ea
Marcus Rothoff <marcusrothoff@hotmail.com>
2022-04-09 16:21:52 +0200

    loaddata into postgres heroku

--- canned_water/settings.py
+++ canned_water/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
-# ALLOWED_HOSTS = ["canned-water.herokuapp.com", "*"]
+# ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ["canned-water.herokuapp.com", "*"]
 
 
 # Application definition
