64ff3379c454848d05e16ad21fb47e7506cc0a9f
Marcus Rothoff <marcusrothoff@hotmail.com>
2022-04-09 22:00:04 +0200

    Add connect - app to settings, include url to main app, and models

--- connect/models.py
+++ connect/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    email = models.EmailField(max_length=70, null=False, blank=False)
+    title = models.CharField(max_length=40, null=False, blank=False)
+    content = models.TextField(max_length=900, null=False, blank=False)
+
+    def __str__(self):
+        return self.title
+
+
