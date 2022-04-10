4a40c7cbef5dac4aa5eb99cb524e5d4fdf65eef1
Marcus Rothoff <marcusrothoff@hotmail.com>
2022-04-10 08:25:24 +0200

    Add JS to base to keep footer always on bottom and readjust itself.

--- static/js/base.js
+++ static/js/base.js
@@ -24,3 +24,20 @@ $(document).ready(function () {
         $(".load-spinner2").fadeOut("fast");
     });
 });
+
+
+    $(document).ready(function() {
+        setInterval(function() {
+            var docHeight = $(window).height();
+            var footerHeight = $('#footer').height();
+            var footerTop = $('#footer').position().top + footerHeight;
+            var marginTop = (docHeight - footerTop + 10);
+
+            if (footerTop < docHeight)
+                $('#footer').css('margin-top', marginTop + 'px'); // padding of 30 on footer
+            else
+                $('#footer').css('margin-top', '0px');
+            // console.log("docheight: " + docHeight + "\n" + "footerheight: " + footerHeight + "\n" + "footertop: " + footerTop + "\n" + "new docheight: " + $(window).height() + "\n" + "margintop: " + marginTop);
+        }, 250);
+    });
+
