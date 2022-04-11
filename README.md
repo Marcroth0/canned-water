59d5d2aa1a8ca7bc372b1027ed7c410b9967e6de
Marcus Rothoff <marcusrothoff@hotmail.com>
2022-04-11 21:25:53 +0200

    Add testing of products and template for readme

--- README.md
+++ README.md
@@ -1,1 +1,240 @@
-# canned-water
+# relationship-quarrel
+
+![amiresponsive]()
+
+Live link: https://quarrel2022.herokuapp.com/
+
+## About:
+
+
+### Table of Contents
+
+1. [User Stories](#user-stories)
+2. [UX](#ux)
+   1. [Strategy](#strategy)
+   2. [Design/Structure](#design/structure)
+   3. [Databases](#databases)
+   4. [Planning](#planning)
+   5. [End Design](#end-design-result)
+3. [Testing](#testing)
+   1. [Manual Testing](#manual-testing)
+   2. [Validator](#validator-testing)
+   3. [Bugs](#unfixed-bugs)
+   4. [Improvements](#improvements)
+4. [Credits](#credits)
+
+### User Stories
+
+
+
+View my User Stories and planning [here](https://github.com/Marcroth0/relationship-quarrel/issues).
+
+# UX
+
+## Strategy
+
+Target Audience:
+
+- 18-40 year olds
+- People who are looking to take arguments with a bit of comedy
+- People who would like to improve their communication skills
+- People who've seen too many one-sided-stories
+
+The audience is looking for something where both sides are visible. With the ability to post both arguments and hash it out once and for all, with complete strangers on the internet, the site allows the user to read different points of views and thereafter see whose argument was the best.
+
+## Design/Structure
+
+
+Apps:
+
+
+
+### Databases:
+
+Quarrelapp holds all the databases, consisting of:
+
+1. Post in combination with CommentPost allows for the ability of letting the user create a post, but through a OneToOneField with CommentPost, separate the likes and comments. Through the ability of calling different keys, connected to each argument, it allows for the possibility of cross-liking as well as future customization of the comment section(more on that in improvements) With each post, the model generates the users post but splits the content one and content two into separate keys which are then callable in views.
+2. Comment allows for users to comment on posts with date-stamps, as well as Post, have a built-in CASCADE in order to delete all comments related to user if user decided to delete their account.
+
+Below is a diagram of the correlation between the models:
+
+![database-schema]()
+
+### Planning
+
+I wireframed it using Balsamiq:
+
+Landing page:
+
+![wireframe]()
+
+Landing page - Mobile:
+
+![wireframe]()
+
+A post full screen:
+
+![wireframe]()
+
+### End Design Result:
+
+Easily navigated navbar containing a home button, disguised as a logo, an about-page, a registration and a login-button that catches the visitors eye.
+
+Navbar:
+![navbar]()
+
+For users that are logged in, the post-button swaps to an account button, containing: My posts, deactivate account, delete account, as well as logout. The register button is replaced with an additional post-button.
+
+Navbar(Logged in):
+![navbar]()
+
+Landing page is a cooperation between the hero-image and the Quarrels-section. Instantly the visitor gets questions answered:
+
+1. What the point of the site is
+2. The possibility of being a part of it
+3. How the posts are portrayed on the landing page
+
+These three points together form a need to be included.
+
+Hero:
+![hero]()
+
+
+Quarrels:
+![quarrels]()
+
+
+Post Detail:
+![postdetail]()
+
+
+
+About page:
+![about]()
+
+
+Footer:
+![footer]()
+
+Whimsical writings:
+
+
+### Color Palette
+
+Colors:
+![palette]()
+
+### Reasoning
+
+The design of the website was crucial. It being a "argument-solver" it needed to be peculiar and not take the initial argument, or the definition of an argument, all that seriously. With the ability of seeing it, instead of an obstacle, as a "funny challenge" to post it to the internet. Hence the sarcastic comments spread around the website. With a comical twist, on serious subjects, it will hopefully invoke a funny tone in the comments as well as an appearance of the "good side" of the internet which are there to actually help, inbetween a sea of funny anti-comments. (Comments mainly meant to ridicule)
+
+### Fonts
+
+Font-family: "Mochiy Pop P One", Montserrat;
+
+##
+
+## CRUD
+
+- The user has the ability to
+  C - reate their own post.
+  R - read their, or others posts and comments.
+  U - update their post
+  D - delete their post and comments, as well as their account.
+
+## Testing
+
+I tested the responsiveness of the site on the below units, using chrome dev as well as some in real life testing:
+
+**Mobile**
+
+- Moto G4 (360x640)
+- Galaxy S5 (360x640)
+- Pixel 2 (411x731)
+- Pixel 2 XL (411x823)
+- iPhone 5/SE (320x568)
+- iPhone 6/7/8 (375x667)
+- iPhone 6/7/8 Plus (414x736)
+- iPhone X (375x812)
+- iPad (768x1024)
+- iPad Pro (1024x1366)
+
+**Browsers:**
+
+- Chrome
+- Mozilla Firefox
+- Safari
+
+### Manual testing
+
+See further on click [here]()
+
+### Lighthouse
+
+![lighthouse]()
+
+### Validator Testing
+
+W3 HTML Validator:
+
+![html]()
+
+W3 CSS Validator:
+
+![css]()
+
+For Python I've used pep8 validator which resulted in 0 errors on all pages.
+
+## Unfixed Bugs
+
+1. There's a bug where if you type one very, very long word, without spaces, in argument one or two, the post_details overflows.
+2. CommentPost (content one and two) aren't successfully deleted from DB. Have to add functionality in already existing delete-function to also include this.
+
+## Technologies used:
+
+- I used [Python](https://www.python.org/) to write my functions and models
+
+- [Django](https://www.djangoproject.com/) is the framework used to build project and its apps
+
+- [Cloudinary](https://cloudinary.com/) has been used to store my images and static files
+
+- [Crispyforms](https://django-crispy-forms.readthedocs.io/en/latest/) has been used to easily display forms
+
+- [Github](https://github.com/) Has been to store and plan project
+
+- [Vscode](https://code.visualstudio.com/) my choice of IDE
+
+- [PostgreSQL](https://www.postgresql.org/) Database
+
+- [SQlite](https://www.sqlite.org/index.html) has been used for local testing
+
+- [Pep8](https://pypi.org/project/autopep8/) has been used for formatting and error-checking python-code
+
+- [Prettier](https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode) has been used for formatting and error-checking html-code
+
+- [ImgBot](https://imgbot.net/) - my trustworthy companion for optimizing size of images
+
+- [Google-Developer-Tools](https://developers.google.com/web/tools) - for debugging
+
+- [Bootstrap4](https://getbootstrap.com/) - css library
+
+- [AmIResponsive](http://ami.responsivedesign.is/#) - used to check responsiveness, and collect image you find at the top of page.
+
+## Features Left to Implement
+
+
+
+## Deployment
+
+The site was deployed using Heroku, following the steps offered by Codeinstitute. Instructions are found [here](https://codeinstitute.s3.amazonaws.com/fst/Django%20Blog%20Cheat%20Sheet%20v1.pdf)
+
+Heroku:
+
+AWS - static:
+
+
+## Credits
+
+- A general shout-out to [StackOverflow](https://stackoverflow.com/), which solved a lot of issues.
+- Credit for images used in posts [chikenbugagashenka](https://www.freepik.com/free-vector/boys-girls-kids-aggression-conflict-set_20892288.htm#query=argue&position=11&from_view=search)
+- [dbdiagram](https://dbdiagram.io/) for offering a database-visualiser
--- products/test_views.py
+++ products/test_views.py
@@ -1,34 +1,222 @@
 from django.test import TestCase, Client
 from django.urls import reverse
-from .models import Product
+from django.contrib.auth.models import User
+from django.contrib.messages import get_messages
+from products.models import Product, Category
 
 
 class TestProductsViews(TestCase):
-    """ Test the products response  """
+    """Test Product views for all types of user"""
 
     def setUp(self):
         self.client = Client()
+        self.user = User.objects.create_user(
+            username='test_user',
+            email='test@test.com',
+            password='testpassword'
+        )
+        self.category = Category.objects.create(
+            name="test_category",
+            friendly_name="Test Category"
+        )
+        self.product = Product.objects.create(
+            sku="123",
+            name="test product",
+            description="test description",
+            price=1200,
+            image="testimg.jpg",
+            category=self.category,
+        )
+        self.all_products = reverse("products")
+        self.product_detail = reverse("product_detail",
+                                      kwargs={"product_id": self.product.id})
 
-    def test_url_response(self):
-        response = self.client.get('/products/')
+        # Views with superuser restrictions
+
+        self.super_user = User.objects.create_superuser(
+            username='test_admin',
+            email='testadmin@test.com',
+            password='testadminpassword'
+        )
+
+        self.add_product = reverse("add_product")
+        self.home = reverse("home")
+        self.edit_product = reverse("edit_product",
+                                    kwargs={"product_id": self.product.id})
+        self.delete_product = reverse("delete_product",
+                                      kwargs={"product_id": self.product.id})
+
+    def test_all_products_view(self):
+        """
+        Test the all products view
+        """
+
+        response = self.client.get(self.all_products)
         self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "products/products.html")
+        self.assertTemplateUsed(response, "base.html")
 
-    def test_view_uses_correct_template(self):
-        """ Test using correct template """
-        response = self.client.get(reverse('products'))
-        self.assertTemplateUsed(response, 'products/products.html')
-        self.assertTemplateNotUsed(response, 'bag/bag.html')
+    def test_all_products_views_with_search(self):
+        """
+        Test the all_products view with a search query parameter
+        """
+
+        response = self.client.get(self.all_products,
+                                   {"q": "test"})
+        context = response.context
+        self.assertTrue(context['search_term'])
+        self.assertEqual(context['search_term'], 'test')
+
+    def test_view_product_detail_view(self):
+        """ Test the product_detail view """
+
+        response = self.client.get(self.product_detail)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "products/product_detail.html")
 
 
-# class TestIndividualProductViews(TestCase):
+    def test_view_product_detail_view(self):
+        """ Test the product_detail view """
 
-#     def setUp(self):
-#         """ Set up test data """
-#         self.client = Client()
-#         self.product = Product.objects.create(pk=1,
-#                                               name="testproduct",
-#                                               price=40.00)
+        response = self.client.get(self.product_detail)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "products/product_detail.html")
+        
+    # Views with user/superuser restrictions
 
-#     def test_view_uses_correct_template(self):
-#         response = self.client.get(f'/products/{self.product.id}/')
-#         self.assertTemplateUsed(response, 'products/product_detail.html')
+    def test_add_product_not_superuser(self):
+        """ Test the add_product view if the user is not a superuser """
+
+        self.client.login(
+            username="test_user", password="testpassword")
+        response = self.client.get(self.add_product)
+        self.assertRedirects(response, self.home)
+        self.assertEqual(response.status_code, 302)
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(str(messages[0]),
+                         'Sorry, you are not a superuser.')
+
+    def test_add_product_superuser_GET(self):
+        """ Test the add_product GET functionality for superusers """
+
+        self.client.login(
+            username="test_admin", password="testadminpassword")
+        response = self.client.get(self.add_product)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "products/add_product.html")
+        self.assertTemplateUsed(response, "base.html")
+
+    def test_add_product_invalidform_POST(self):
+        """ Test the add_product view using an invalid form """
+
+        self.client.login(
+            username="test_admin", password="testadminpassword")
+        response = self.client.post(self.add_product, {
+            "category": self.category,
+            "sku": "",
+            "name": "",
+            "description": "",
+            "price": "",
+        })
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(str(messages[0]),
+                         'Failed to add product. Please ensure the form is valid.')
+
+    def test_add_product_validform_POST(self):
+        """ Test the add_product view with a valid form """
+
+        self.client.login(
+            username="test_admin", password="testadminpassword")
+        response = self.client.post(self.add_product, {
+            "name": "Test product",
+            "description": "test description",
+            "price": 1200,
+        })
+        product = Product.objects.get(name="Test product")
+        self.assertTrue(product)
+        self.assertEqual(product.description, "test description")
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(str(messages[0]),
+                         'Successfully added product!')
+
+    def test_edit_product_not_superuser(self):
+        """ Test the edit_product view if the user is not a superuser """
+
+        self.client.login(
+            username="test_user", password="testpassword")
+        response = self.client.get(self.edit_product)
+        self.assertRedirects(response, self.home)
+        self.assertEqual(response.status_code, 302)
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(str(messages[0]),
+                         'Sorry, you are not a superuser.')
+
+    def test_edit_product_superuser_GET(self):
+        """ Test the edit_product GET functionality if superuser """
+
+        self.client.login(
+            username="test_admin", password="testadminpassword")
+        response = self.client.get(self.edit_product)
+        self.assertEqual(response.context['form'].initial['name'],
+                         self.product.name)
+        self.assertEqual(response.context['form'].initial['description'],
+                         self.product.description)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "products/edit_product.html")
+        self.assertTemplateUsed(response, "base.html")
+
+    def test_edit_product_invalidform_POST(self):
+        """ Test the edit_product view post with an invalid form"""
+
+        self.client.login(
+            username="test_admin", password="testadminpassword")
+        response = self.client.post(self.edit_product, {
+            "name": "",
+            "description": "",
+            "price": "",
+        })
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(str(messages[0]),
+                         'Failed to update product.\
+ Please check the form is valid.')
+
+    def test_edit_product_validform_POST(self):
+        """ Test edit_product view with a valid form """
+
+        self.client.login(
+            username="test_admin", password="testadminpassword")
+        response = self.client.post(self.edit_product, {
+            "name": "Test edited product",
+            "description": "test edited description",
+            "price": 1300,
+        }
+        )
+        product = Product.objects.get(id=self.product.id)
+        self.assertEqual(product.description, 'test edited description')
+        self.assertEqual(product.price, 1300)
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(str(messages[0]),
+                         'Successfully updated item!')
+
+    def test_delete_product_not_superuser(self):
+        """ Test delete_product view if user is not superuser """
+
+        self.client.login(
+            username="test_user", password="testpassword")
+        response = self.client.get(self.delete_product)
+        self.assertRedirects(response, self.home)
+        self.assertEqual(response.status_code, 302)
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(str(messages[0]),
+                         'Sorry, you are not a superuser.')
+
+    def test_delete_product_superuser_GET(self):
+        """ Test delete_product view GET functionality if a superuser """
+
+        self.client.login(
+            username="test_admin", password="testadminpassword")
+        response = self.client.get(self.delete_product)
+        self.assertRedirects(response, self.all_products)
+        messages = list(get_messages(response.wsgi_request))
+        self.assertEqual(str(messages[0]),
+                         'Product deleted!')
