272ad48f3b03121f2be065f24556a7cc778a1c6f
Marcus Rothoff <marcusrothoff@hotmail.com>
2022-05-18 00:27:51 +0200

    test webhook

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .webhooks import webhook
+from . import webhooks
 
 
 
@@ -8,5 +8,5 @@ urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout_confirmation/<order_number>', views.checkout_confirmation, name='checkout_confirmation'),
     path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
-    path('wh/', views.webhook, name='webhook'),
+    path('wh/', webhooks.webhook, name='webhook'),
 ]
