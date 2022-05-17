d6efee275f27f091eb61570ed8a1a5dfef84c288
Marcus Rothoff <marcusrothoff@hotmail.com>
2022-05-18 00:18:35 +0200

    test

--- checkout/urls.py
+++ checkout/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('checkout_confirmation/<order_number>', views.checkout_confirmation, name='checkout_confirmation'),
     path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
-    path('wh/', webhook, name='webhook'),
+    path('wh/', views.webhook, name='webhook'),
 ]
